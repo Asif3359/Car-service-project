7144a0b4cf57d4d9798aea4671951f94cc631414
Asif359 <asifahammednishst@gmail.com>
2023-10-23 16:15:09 +0600

    added footer , banner

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Outlet } from 'react-router-dom'
+import NavBar from './Pages/Shared/NavBar/NavBar'
+import Footer from './Pages/Shared/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <div>
-      <Outlet></Outlet>
-     </div>
+      <div className='container mx-auto'>
+        <NavBar></NavBar>
+        <Outlet></Outlet>
+        <Footer></Footer>
+      </div>
     </>
   )
 }
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import Baner from "../Baner/Baner";
+
 
 const Home = () => {
     return (
         <div>
+            <Baner></Baner>
             This is Home
+            
         </div>
     );
 };
