92e0a469aef2dde8449590c42eb5e8cd14f282e1
Asif359 <asifahammednishst@gmail.com>
2023-10-31 00:53:36 +0600

    added patch funcitionality

--- src/Pages/Bookings/Booking.jsx
+++ src/Pages/Bookings/Booking.jsx
@@ -1,28 +1,33 @@
-const Booking = ({ booking }) => {
+const Booking = ({ booking,handleDelete,handleConfirm }) => {
     console.log(booking);
-    const {customerName,service,date,email,name,img,price}=booking;
+    const {_id, customerName, service, date, email, name, img, price,status } = booking;
+
+   
     return (
         <>
             <tr>
                 <th>
-                    <label>
-                        <input type="checkbox" className="checkbox" />
-                    </label>
+                    <button onClick={()=>handleDelete(_id)} className="btn btn-square btn-outline">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                    </button>
                 </th>
                 <td>
                     <div className="flex items-center space-x-3">
                         <div className="avatar ">
                             <div className="rounded w-12 h-12">
-                                <img src={img} alt={img?`${service}`:"No Image"} />
+                                <img src={img} alt={img ? `${service}` : "No Image"} />
                             </div>
                         </div>
-                    </div> 
+                    </div>
                 </td>
                 <td>{service}</td>
                 <td>${price}</td>
                 <td>${date}</td>
                 <th>
-                    <button className="btn btn-ghost btn-xs">details</button>
+                    {
+                        status ==='confirm'?<span className="font-bold">Confirmed</span>:<button onClick={()=>handleConfirm(_id)} className="btn btn-ghost btn-xs">please Confirm</button>
+                    }
+                    
                 </th>
             </tr>
         </>
--- src/Pages/Bookings/Bookings.jsx
+++ src/Pages/Bookings/Bookings.jsx
@@ -15,8 +15,58 @@ const Bookings = () => {
                 console.log(data);
                 setBookings(data);
             })
-    }, [])
+    }, []);
+    const handleDelete =(_id)=>{
+        const proceed = confirm('Are You sure you Want to DELETE');
+        if(proceed)
+        {
+            fetch(`http://localhost:5000/bookings/${_id}`,{
+                method:"DELETE",
 
+
+            })
+            .then(res=>res.json())
+            .then(data=>{
+                console.log(data);
+                if(data.deletedCount>0)
+                {
+                    alert("Delete Successfully");
+                    const remaining = bookings.filter(booking=>booking._id !== _id );
+                    setBookings(remaining);
+
+                }
+            })
+        }
+    }
+    const handleConfirm =(_id)=>{
+        const proceed = confirm('Are You sure you Want to DELETE');
+        if(proceed)
+        {
+            fetch(`http://localhost:5000/bookings/${_id}`,{
+                method:"PATCH",
+                headers:{
+                    'content-type':'application/json'
+                },
+                body:JSON.stringify({status:"confirm"})
+
+
+            })
+            .then(res=>res.json())
+            .then(data=>{
+                console.log(data);
+                if(data.modifiedCount>0)
+                {
+                    alert("update Successfully");
+                    const remaining = bookings.filter(booking=>booking._id !== _id );
+                    const updated = bookings.find(booking=>booking._id === _id );
+                    updated.status="confirm";
+                    const newBookings = [updated,...remaining];
+                    setBookings(newBookings);
+
+                }
+            })
+        }
+    }
     return (
         <div>
             <h1 className='text-3xl'>{bookings.length}</h1>
@@ -26,9 +76,7 @@ const Bookings = () => {
                     <thead>
                         <tr>
                             <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
+                               Remove
                             </th>
                             <th>Image</th>
                             <th>Service</th>
@@ -38,7 +86,7 @@ const Bookings = () => {
                     </thead>
                     <tbody>
                         {
-                            bookings.map((booking, index) => <Booking key={index} booking={booking}></Booking>)
+                            bookings.map((booking, index) => <Booking key={index} booking={booking} handleDelete={handleDelete} handleConfirm={handleConfirm}></Booking>)
                         }
                     </tbody>
 
