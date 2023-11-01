36775e096ca80ded805885b6c5112fdb7732795c
Asif359 <asifahammednishst@gmail.com>
2023-11-01 23:29:31 +0600

    added hook on useAxios

--- src/Pages/Bookings/Bookings.jsx
+++ src/Pages/Bookings/Bookings.jsx
@@ -2,103 +2,106 @@ import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../Providers/AuthProviders';
 import Booking from './Booking';
 import axios from 'axios';
+import useAxios from '../../Hooks/useAxios';
 
 const Bookings = () => {
     const { user } = useContext(AuthContext);
     const [bookings, setBookings] = useState([]);
+    const useAxiosSecure = useAxios();
 
-    const url = `http://localhost:5000/bookings?email=${user?.email}`
+    const url = `/bookings?email=${user?.email}`
+    // const url = `http://localhost:5000/bookings?email=${user?.email}`
 
     useEffect(() => {
-        axios.get(url,{withCredentials:true})
-        .then(res=>{
-            setBookings(res.data);
-        })
-        // fetch(url)
-        //     .then(res => res.json())
-        //     .then(data => {
-        //         console.log(data);
-        //         setBookings(data);
-        //     })
-    }, []);
-    const handleDelete =(_id)=>{
-        const proceed = confirm('Are You sure you Want to DELETE');
-        if(proceed)
-        {
-            fetch(`http://localhost:5000/bookings/${_id}`,{
-                method:"DELETE",
+        // axios.get(url,{withCredentials:true})
+        // .then(res=>{
+        //     setBookings(res.data);
+        // })
+        // // fetch(url)
+        // //     .then(res => res.json())
+        // //     .then(data => {
+        // //         console.log(data);
+        // //         setBookings(data);
+        // //     })
+        useAxiosSecure.get(url)
+            .then(res => {
+                    setBookings(res.data);
+                })
+            }, [url,useAxiosSecure]);
+        const handleDelete = (_id) => {
+            const proceed = confirm('Are You sure you Want to DELETE');
+            if (proceed) {
+                fetch(`http://localhost:5000/bookings/${_id}`, {
+                    method: "DELETE",
 
 
-            })
-            .then(res=>res.json())
-            .then(data=>{
-                console.log(data);
-                if(data.deletedCount>0)
-                {
-                    alert("Delete Successfully");
-                    const remaining = bookings.filter(booking=>booking._id !== _id );
-                    setBookings(remaining);
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            alert("Delete Successfully");
+                            const remaining = bookings.filter(booking => booking._id !== _id);
+                            setBookings(remaining);
 
-                }
-            })
-        }
-    }
-    const handleConfirm =(_id)=>{
-        const proceed = confirm('Are You sure you Want to DELETE');
-        if(proceed)
-        {
-            fetch(`http://localhost:5000/bookings/${_id}`,{
-                method:"PATCH",
-                headers:{
-                    'content-type':'application/json'
-                },
-                body:JSON.stringify({status:"confirm"})
-
-
-            })
-            .then(res=>res.json())
-            .then(data=>{
-                console.log(data);
-                if(data.modifiedCount>0)
-                {
-                    alert("update Successfully");
-                    const remaining = bookings.filter(booking=>booking._id !== _id );
-                    const updated = bookings.find(booking=>booking._id === _id );
-                    updated.status="confirm";
-                    const newBookings = [updated,...remaining];
-                    setBookings(newBookings);
-
-                }
-            })
-        }
-    }
-    return (
-        <div>
-            <h1 className='text-3xl'>{bookings.length}</h1>
-            <div className="overflow-x-auto">
-                <table className="table">
-                    {/* head */}
-                    <thead>
-                        <tr>
-                            <th>
-                               Remove
-                            </th>
-                            <th>Image</th>
-                            <th>Service</th>
-                            <th>Price </th>
-                            <th>Date</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            bookings.map((booking, index) => <Booking key={index} booking={booking} handleDelete={handleDelete} handleConfirm={handleConfirm}></Booking>)
                         }
-                    </tbody>
+                    })
+            }
+        }
+        const handleConfirm = (_id) => {
+            const proceed = confirm('Are You sure you Want to DELETE');
+            if (proceed) {
+                fetch(`http://localhost:5000/bookings/${_id}`, {
+                    method: "PATCH",
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify({ status: "confirm" })
 
-                </table>
+
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.modifiedCount > 0) {
+                            alert("update Successfully");
+                            const remaining = bookings.filter(booking => booking._id !== _id);
+                            const updated = bookings.find(booking => booking._id === _id);
+                            updated.status = "confirm";
+                            const newBookings = [updated, ...remaining];
+                            setBookings(newBookings);
+
+                        }
+                    })
+            }
+        }
+        return (
+            <div>
+                <h1 className='text-3xl'>{bookings.length}</h1>
+                <div className="overflow-x-auto">
+                    <table className="table">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                <th>
+                                    Remove
+                                </th>
+                                <th>Image</th>
+                                <th>Service</th>
+                                <th>Price </th>
+                                <th>Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                bookings.map((booking, index) => <Booking key={index} booking={booking} handleDelete={handleDelete} handleConfirm={handleConfirm}></Booking>)
+                            }
+                        </tbody>
+
+                    </table>
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    };
 
-export default Bookings;
+    export default Bookings;
